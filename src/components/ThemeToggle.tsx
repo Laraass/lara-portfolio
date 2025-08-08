7dbe5673b35c7603bccc96a1d8da9358009a2ac1
Lara <lara.salman01@hotmail.com>
2025-08-08 23:37:03 +0200

    chore: update themetoggle component

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,39 +1,26 @@
-import { useState, useEffect } from "react";
 import { Icon } from "@iconify/react";
 
-export default function ThemeToggle() {
-  const [theme, setTheme] = useState<"light" | "dark">("light");
+interface Props {
+  theme: "light" | "dark";
+  toggleTheme: () => void;
+}
 
-  useEffect(() => {
-    const saved = localStorage.getItem("theme");
-    if (saved === "dark" || saved === "light") {
-      setTheme(saved);
-    }
-  }, []);
-
-  useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme);
-    localStorage.setItem("theme", theme);
-  }, [theme]);
-
+export default function ThemeToggle({ theme, toggleTheme }: Props) {
   return (
     <button
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      onClick={toggleTheme}
       aria-label="Toggle dark/light mode"
-      className="
-         transition
-        flex items-center justify-center
-      "
+      className="transition flex items-center justify-center"
     >
       {theme === "light" ? (
         <Icon
           icon="stash:moon-solid"
-          className="w-6 h-6 text-[var(--color-primary)] hover:text-[var(--color-primary-subtle)] transition-colors cursor-pointer"
+          className="size-6 text-[var(--color-primary)] hover:text-[var(--color-primary-subtle)] transition-colors cursor-pointer"
         />
       ) : (
         <Icon
           icon="mingcute:sun-fill"
-          className="w-6 h-6 text-[var(--color-secondary)] hover:text-[var(--color-secondary-bold)] transition-colors cursor-pointer"
+          className="size-6 text-[var(--color-secondary)] hover:text-[var(--color-secondary-bold)] transition-colors cursor-pointer"
         />
       )}
     </button>
