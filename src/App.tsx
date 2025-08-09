91c8858d842641b97f56b7407a5b902a99974a22
Lara <lara.salman01@hotmail.com>
2025-08-09 16:50:17 +0200

    chore: mini fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./sections/Home";
 import About from "./sections/About";
