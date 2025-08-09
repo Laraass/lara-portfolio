a861a23f33740a0fbd117db6a8126a8189f44d31
Lara <lara.salman01@hotmail.com>
2025-08-09 20:01:23 +0200

    chore: remove rounded corners in about card

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -10,7 +10,7 @@ interface AboutCardProps {
 const AboutCard: React.FC<AboutCardProps> = ({ title, text }) => {
   return (
     <div
-      className="flex flex-col gap-2 bg-[var(--bg)] text-[var(--text)] rounded-xl p-4 max-w-3xl mx-auto
+      className="flex flex-col gap-2 bg-[var(--bg)] text-[var(--text)] p-4 max-w-3xl mx-auto
         shadow-[0_-5px_10px_-3px_#0000001a,_0_10px_20px_-3px_#0000000d,_0_4px_6px_-2px_#0000000d]
 "
     >
