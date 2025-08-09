23775804cc1fbf8f4c4b129643d1f857c7d24d63
Lara <lara.salman01@hotmail.com>
2025-08-09 03:32:11 +0200

    chore: change navbar position on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,10 +11,10 @@ export default function Navbar({ theme, isMobile, toggleTheme }: Props) {
   const baseClasses = `flex flex-row justify-between items-center w-full backdrop-blur-md`;
   const bgClasses = `bg-[var(--bg)] bg-opacity-80`;
   const paddingClasses = isMobile ? "px-3 py-4" : "p-6";
-  const strokeTop = isMobile ? `border-t border-t-secondary` : "";
+  const strokeBottom = isMobile ? "border-b border-b-secondary" : "";
 
   const positionClasses = isMobile
-    ? "fixed bottom-0 left-0 w-full z-50"
+    ? "fixed top-0 left-0 w-full z-50"
     : "sticky top-0 left-0 w-full z-50";
 
   const iconClasses = `
@@ -25,49 +25,69 @@ export default function Navbar({ theme, isMobile, toggleTheme }: Props) {
   `;
 
   return (
-  <nav
-    className={`${baseClasses} ${bgClasses} ${paddingClasses} ${strokeTop} ${positionClasses}`}
-    style={{ WebkitBackdropFilter: "blur(5px)", backdropFilter: "blur(5px)" }}
-  >
-    <a href="/" className="font-display text-[var(--text)] text-2xl hover:text-secondary">
-      Lara Salman
-    </a>
+    <nav
+      className={`${baseClasses} ${bgClasses} ${paddingClasses} ${strokeBottom} ${positionClasses}`}
+      style={{ WebkitBackdropFilter: "blur(5px)", backdropFilter: "blur(5px)" }}
+    >
+      <a href="/" className="font-display text-[var(--text)] text-2xl hover:text-secondary">
+        Lara Salman
+      </a>
 
-    {isMobile ? (
-      <div className="flex flex-row gap-6 items-center">
-        {/* Socials */}
-        <a href="https://www.linkedin.com/in/lara-salman-00224b1b2/" target="_blank" rel="noopener noreferrer" className={iconClasses}>
-          <Icon icon="ri:linkedin-fill" className="size-6" />
-        </a>
-        <a href="https://github.com/Laraass" target="_blank" rel="noopener noreferrer" className={iconClasses}>
-          <Icon icon="mdi:github" className="size-6" />
-        </a>
-        {/* add cv later! */}
-        <a href="/" target="_blank" rel="noopener noreferrer" className={iconClasses}>
-          <Icon icon="tabler:file-cv-filled" className="size-6" />
-        </a>
-
-        {/* ThemeToggle */}
-        <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
-      </div>
-    ) : (
-      <>
-        <div className="flex flex-row gap-4">
-          <a href="https://www.linkedin.com/in/lara-salman-00224b1b2/" target="_blank" rel="noopener noreferrer" className={iconClasses}>
+      {isMobile ? (
+        <div className="flex flex-row gap-6 items-center">
+          {/* Socials */}
+          <a
+            href="https://www.linkedin.com/in/lara-salman-00224b1b2/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={iconClasses}
+          >
             <Icon icon="ri:linkedin-fill" className="size-6" />
           </a>
-          <a href="https://github.com/ditt-användarnamn" target="_blank" rel="noopener noreferrer" className={iconClasses}>
+          <a
+            href="https://github.com/Laraass"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={iconClasses}
+          >
             <Icon icon="mdi:github" className="size-6" />
           </a>
           {/* add cv later! */}
-          <a href="/" target="_blank" rel="noopener noreferrer" className={iconClasses}> 
+          <a href="/" target="_blank" rel="noopener noreferrer" className={iconClasses}>
             <Icon icon="tabler:file-cv-filled" className="size-6" />
           </a>
-        </div>
 
-        <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
-      </>
-    )}
-  </nav>
-);
+          {/* ThemeToggle */}
+          <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
+        </div>
+      ) : (
+        <>
+          <div className="flex flex-row gap-4">
+            <a
+              href="https://www.linkedin.com/in/lara-salman-00224b1b2/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={iconClasses}
+            >
+              <Icon icon="ri:linkedin-fill" className="size-6" />
+            </a>
+            <a
+              href="https://github.com/ditt-användarnamn"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={iconClasses}
+            >
+              <Icon icon="mdi:github" className="size-6" />
+            </a>
+            {/* add cv later! */}
+            <a href="/" target="_blank" rel="noopener noreferrer" className={iconClasses}>
+              <Icon icon="tabler:file-cv-filled" className="size-6" />
+            </a>
+          </div>
+
+          <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
+        </>
+      )}
+    </nav>
+  );
 }
